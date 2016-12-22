b75b4f9e6458ec5c3d274339f689b7611183a9a3
matus <ixcoatl.perez.de.la.b@gmail.com>
2016-12-21 18:35:34 -0600

    some init msg

---
+++ com/iq3/IQ3_UI.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iq3;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ *
+ * @author matus
+ */
+@Theme("tema_iu7")
+public class IQ3_UI extends UI
+{
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest)
+    {
+        final VerticalLayout layout = new VerticalLayout();
+
+        final TextField name = new TextField();
+        name.setCaption("Escriba algo aquí:");
+
+        Button button = new Button("Dime que escribí");
+
+        button.addClickListener(new Button.ClickListener()
+        {
+            @Override
+            public void buttonClick(Button.ClickEvent event)
+            {
+                System.out.println("Click!");
+                layout.addComponent(new Label("Usted escribió> " + name.getValue()));
+            }
+        });
+
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+
+        setContent(layout);
+    }
+}
